a4a9264f0640d5c567dd54ce00c3f7f05664ffa6
kimhyun <forhn37@gmail.com>
2023-10-28 15:30:47 +0900

    new 연산자 없이 호출한 this가 전역객체가 됨

---
+++ foo.js
@@ -0,0 +1,13 @@
+function Circle(radius) {
+  this.radius = radius;
+  this.getDiameter = function () {
+    return 2 * this.radius;
+  }
+}
+
+const circle = Circle(5);
+
+console.log(circle);
+
+console.log(radius);
+console.log(getDiameter());
